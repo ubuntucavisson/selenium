605fc5d38b1545d96fc191f213c14fbe15af6a5b
ubuntucavisson <ubuntucavisson@gmail.com>
2021-07-08 13:48:07 +0530

    MvnProj backup on 8th july

---
+++ MvnProject/src/test/java/helloWorld/helloWorldClass.java
@@ -0,0 +1,32 @@
+package helloWorld;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class helloWorldClass {
+	
+	@Test
+	public void helloWorldPrint() {
+		System.out.println("... hello world from maven project");
+		
+		String projectPath = System.getProperty("user.dir");
+		System.setProperty("webdriver.chrome.driver", "D:\\PD\\chromedriver_win32\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("http://www.google.com");
+		driver.findElement(By.xpath("//input[@class='gLFyf gsfi']")).sendKeys("abcd");
+		//driver.findElement(By.name("btnk")).sendKeys(Keys.RETURN);
+		//driver.findElement(By.name("btnk")).click();
+		WebElement button = driver.findElement(By.xpath("//body/div[1]/div[3]/form[1]/div[1]/div[1]/div[3]/center[1]/input[1]"));
+		
+		button.sendKeys(Keys.RETURN);
+		driver.close();
+		
+		System.out.println("projectPath is "+projectPath);
+	}
+
+}
